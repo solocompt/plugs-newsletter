5024ff397bf29494bf58ea22449d222960aa970f
Ricardo Lobo <ricardolobo@audienciazero.org>
2017-01-11 17:56:07 +0000

    adds / removes newsletter subscription on user update

--- plugs_newsletter/models.py
+++ plugs_newsletter/models.py
@@ -1,15 +1,19 @@
 """
 Plugs Newsletter Models
 """
-
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError, transaction
 
+from plugs_newsletter.settings import plugs_newsletter_settings
 from plugs_core import mixins
 from plugs_mail.utils import to_email 
-
 from plugs_newsletter import emails
+
 
 class Subscription(mixins.Timestampable, models.Model):
     """
@@ -43,3 +47,29 @@ class Subscription(mixins.Timestampable, models.Model):
         """
         verbose_name = _('subscription')
         verbose_name_plural = _('subscriptions')
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def update_newsletter_settings(instance, created, **kwargs):
+    """
+    Every time the user is saved we need to check the newsletter flag
+    and update it accordingly
+    """
+    send_newsletter = getattr(instance, plugs_newsletter_settings['NEWSLETTER_ENDPOINT'])
+    if send_newsletter:
+        kwargs = {
+            'email': instance.email,
+            'first_name': instance.first_name,
+            'last_name': instance.last_name
+        }
+        try:
+            with transaction.atomic():
+                subscription = Subscription.objects.create(**kwargs)
+        except IntegrityError:
+             pass
+    else:
+        try:
+            subscription = Subscription.objects.get(email=instance.email)
+            subscription.delete()
+        except Subscription.DoesNotExist:
+            pass
